31c668d580099e32b026c554d35ed14433b46bc0
seth <seth@ecoflow.com>
2026-01-09 16:37:54 +0800

    Fix navigation visibility: add app_name to urls.py and add icon to menu item

--- netbox_save_download/navigation.py
+++ netbox_save_download/navigation.py
@@ -1,10 +1,9 @@
 from netbox.plugins import PluginMenuItem
 
-# 定义菜单项
-home_item = PluginMenuItem(
-    link="plugins:netbox_save_download:home",
-    link_text="Save and Download Config",
+menu_items = (
+    PluginMenuItem(
+        link="plugins:netbox_save_download:home",
+        link_text="Save and Download Config",
+        icon_class="mdi mdi-cloud-download",
+    ),
 )
-
-# 直接导出 menu_items 元组
-menu_items = (home_item,)
--- netbox_save_download/urls.py
+++ netbox_save_download/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import view
 
+app_name = 'netbox_save_download'
+
 urlpatterns = [
     path('', view.SaveDownloadHomeView.as_view(), name='home'),
     path('download/<str:ip>/', view.DownloadConfigView.as_view(), name='download_config'),
